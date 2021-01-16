d239504b351aac48d208e5c64b68407f1d48e5ab
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2021-01-16 17:16:08 -0600

    can connect from php file to javascript

--- data.php
+++ data.php
@@ -1,4 +1,22 @@
 <?php
 
-echo "Hello World";
-?>
+$dbhost = "localhost";
+    $dbuser = "id15818364_rhuehnergarth";
+    $dbpass = "Ck#W<ql{j@3L[Lnu";
+    $db = "id15818364_whitecastlechores";
+
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
+
+    $result = mysqli_query($conn, "Select * From HOUSE_MEMBERS")
+
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)){
+        $data[] = row;
+    }
+    echo json_encode($data);
+    ?>
