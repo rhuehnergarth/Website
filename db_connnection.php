b7d525397fedadaed50fd500188892cb0b5b4ae8
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2021-01-16 14:40:29 -0600

    added ajax

--- db_connnection.php
+++ db_connnection.php
@@ -1,18 +1,16 @@
 <?php
 function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "rhuehnergarth";
- $dbpass = "G-Chr1s=U40LuG>J";
- $db = "id15818364_whitecastlechores";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- return $conn;
- }
- 
-function CloseCon($conn)
- {
- $conn -> close();
- }
-   
+{
+    $dbhost = "localhost";
+    $dbuser = "rhuehnergarth";
+    $dbpass = "G-Chr1s=U40LuG>J";
+    $db = "id15818364_whitecastlechores";
+
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
+}
 ?>
