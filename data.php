7e5ed3a85e4da7e7a009b297a1515854fe508b45
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2021-01-16 18:41:19 -0600

    can connect to database and change html values

--- data.php
+++ data.php
@@ -1,22 +1,26 @@
 <?php
 
-$dbhost = "localhost";
+    $dbhost = "localhost";
     $dbuser = "id15818364_rhuehnergarth";
     $dbpass = "Ck#W<ql{j@3L[Lnu";
     $db = "id15818364_whitecastlechores";
 
-    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    
+    $mysqli = new mysqli($dbhost,$dbuser,$dbpass,$db);
+
     // Check connection
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
         exit();
     }
 
-    $result = mysqli_query($conn, "Select * From HOUSE_MEMBERS")
+    // Perform query
+    $result = $mysqli->query("SELECT * FROM HOUSE_MEMBERS");
 
     $data = array();
-    while ($row = mysqli_fetch_assoc($result)){
-        $data[] = row;
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
     }
-    echo json_encode($data);
-    ?>
+    $myJSON = json_encode($data);
+    echo $myJSON;
+?>
